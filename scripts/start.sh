4731d8e8c9f3bc437c176cdd692c1f870e87cb46
Victor da Costa <victorockeiro@gmail.com>
2016-11-01 22:52:03 +0100

    Enable Services and start

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
+## Create file to make sure that services will start
 echo > /var/run/storageos/bootmode_normal
+
+## Trigger network configuration
 /opt/storageos/bin/netconfig
+
+## Enable services to start automatically
+systemctl enable storageos-installer.service
+systemctl enable boot-ovfenv.service
+systemctl enable nginx.service
+systemctl enable storageos-sys.service
+systemctl enable storageos-db.service
+systemctl enable storageos-geodb.service
+systemctl enable storageos-geo.service
+systemctl enable storageos-api.service
+systemctl enable storageos-auth.service
+systemctl enable storageos-controller.service
+systemctl enable storageos-coordinator.service
+systemctl enable storageos-portal.service
+systemctl enable storageos-sa.service
+
+
+## Start services
 systemctl start storageos-installer.service
 systemctl start boot-ovfenv.service
 systemctl start nginx.service
