da7f88cdea6ab970cc417ae740048afdfde9bb3b
Victor da Costa <victorockeiro@gmail.com>
2016-11-01 22:06:09 +0100

    Disable Autostart of CoprHD Services
    Required to make sure that services are started only after network
    configuration customization.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 # install CoprHD and do not start services
 # simplify CI/CD integration if services are not started, since network configuration might be done afterwards
-RUN DO_NOT_START="yes" rpm -iv /build/coprhd-controller/build/RPMS/x86_64/storageos-*.x86_64.rpm
+DO_NOT_START="yes" rpm -iv /build/coprhd-controller/build/RPMS/x86_64/storageos-*.x86_64.rpm
+
+systemctl disable storageos-installer.service
+systemctl disable boot-ovfenv.service
+systemctl disable nginx.service
+systemctl disable storageos-sys.service
+systemctl disable storageos-db.service
+systemctl disable storageos-geodb.service
+systemctl disable storageos-geo.service
+systemctl disable storageos-api.service
+systemctl disable storageos-auth.service
+systemctl disable storageos-controller.service
+systemctl disable storageos-coordinator.service
+systemctl disable storageos-portal.service
+systemctl disable storageos-sa.service
